0dc7fc7a7b0fb4c37d25e50d7e975479b584ecd3
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-02-02 15:41:51 -0500

    browserRouter installed, Setting up Component flow

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeHeader from '';
+
+const Home = () => {
+    return (
+        <div>
+            <HomeHeader />
+            <HomeImg />
+            {/* <NavBar /> */}
+        </div>
+    );
+}
+
+export default Home;
